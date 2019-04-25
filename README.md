6f1daa4b9a79240195aa14987bb7a08e6b2d9d93
Jehaann Manzanares <jehaann07carlos@gmail.com>
2019-04-25 17:38:51 -0600

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+
+#Maquetacion modal con animaciones -> Marca Nike
+![Desing page Modal Nike](https://assets.materialup.com/uploads/06dac6cd-19a7-4027-8b03-dbeecd3c0ad2/preview.jpg)
+
+## Diseño por : [quangnef](https://www.uplabs.com/posts/ui-daily-16)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-function logonike(){
 
+const $overlay = document.getElementById('overlay');
+const $showModal =document.getElementById('modal');
+
+function logonike(){
+    
+
+    
+    $showModal.style.animation = 'animationIn 1s forwards';
+    $overlay.classList.add('active');
+
+}
+
+function HideModal(){
+    
+    $showModal.style.animation = 'animationOut .8s forwards';
+    $overlay.style.transitionDelay = '.8s';
+    $overlay.classList.remove('active');
+    
 }
